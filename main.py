1d6294872f47b815c27cc560197a874ddeefa9bc
Zac Paulson <zpaulson001@gmail.com>
2024-05-20 16:52:07 -0700

    Test adding user to db

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI
 from dotenv import load_dotenv
+import schemas
 import os
 
+from requests import Session
+
+from database import Base, SessionLocal, engine
+from models import User
+
 load_dotenv(".env")
+database_url = os.getenv("DATABASE_URL")
+
+# initialize db
+Base.metadata.create_all(bind=engine)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
 
 app = FastAPI()
 
@@ -10,3 +29,12 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "look out world 👀"}
+
+
+@app.post("/users", response_model=schemas.User)
+async def create_users(db: Session = Depends(get_db)):
+    user = User(email="me@me.com")
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+    return user
